c6ef2e39835bdd6501c4ffc15b7aa7a45091b704
golgarin <golgarin@8f880e6a-a396-11de-8a4e-6d34bb13bdfc>
2009-09-18 08:51:53 +0000

    added comments

--- print_id3.py
+++ print_id3.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+# Simple print ID-Tag of MP3 audio file
+# For testing the mutagen library.
+# Copyright 2009 Daniel Bachmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
 from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import ID3 
